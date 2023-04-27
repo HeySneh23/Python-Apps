73dc97dddaf098d2aa9eada34721a88423ba385a
SnehaCodes <126675039+HeySneh23@users.noreply.github.com>
2023-04-27 23:46:02 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+while True:
+    print("1 Addition")
+    # Complete the rest of the input choices
+
+    choice=input("enter your choice:")
+    # Complete the input
+
+    match choice:
+        case "1":
+            print("add")
+        # Repeat the cases for other choices sub/mul/div
+
+        # use default to break from the loop - Think How
+        case default:
+            print("This is default case")
+
+
